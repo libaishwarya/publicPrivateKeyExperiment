d9cdbe4234f38085119264af4c319931a6eb95f3
Aishwarya <libaishwarya@gmail.com>
2023-03-09 22:58:14 +0530

    added more tryouts

--- pubPriv.py
+++ pubPriv.py
@@ -53,3 +53,45 @@ decrypted_message = private_key.decrypt(
 # Print the original message and decrypted message
 print("Original message:", message)
 print("Decrypted message:", decrypted_message)
+
+
+
+# Generate an RSA private key with a key length of 2048 bits
+private_key2 = rsa.generate_private_key(
+    public_exponent=65537,
+    key_size=2048
+)
+
+# Serialize the private key to PEM format
+private_key_pem2 = private_key2.private_bytes(
+    encoding=serialization.Encoding.PEM,
+    format=serialization.PrivateFormat.PKCS8,
+    encryption_algorithm=serialization.NoEncryption()
+)
+
+# Generate the corresponding public key
+public_key2 = private_key2.public_key()
+
+# Serialize the public key to PEM format
+public_key_pem2 = public_key2.public_bytes(
+    encoding=serialization.Encoding.PEM,
+    format=serialization.PublicFormat.SubjectPublicKeyInfo
+)
+
+# Print the private and public keys in PEM format
+print("Private Key2:\n", private_key_pem2.decode())
+print("Public Key2:\n", public_key_pem2.decode())
+
+# Decrypt the encrypted message using the private key
+decrypted_message = private_key2.decrypt(
+    encrypted_message,
+    padding.OAEP(
+        mgf=padding.MGF1(algorithm=hashes.SHA256()),
+        algorithm=hashes.SHA256(),
+        label=None
+    )
+)
+
+# Print the original message and decrypted message
+print("Original message:", message)
+print("Decrypted message:", decrypted_message)
